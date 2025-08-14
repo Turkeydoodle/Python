467669642ac07b77137c9aa2cf1b7bf527558183
Kevin Todd-Yu <kevintoddyu@gmail.com>
2025-08-14 07:26:58 -0700

    Finishing up on the daily planner.

--- dp.py
+++ dp.py
@@ -10,5 +10,20 @@ for i in range(23):
 def user():
     indata = input('What would you like to do? (e.g. edit, view, or quit)')
     if indata == 'edit':
-        inhour = input('What hour would you like to edit? (e.g. the first time, )')
+        inhour = input('What hour would you like to edit? (e.g. the first time, 0000 for midnight, 1300 for 1pm)')
+        for i in range(23):
+            if plan[i][0] == inhour:
+                newtask = input('What would you like to change the task to?')
+                plan[i][1] = newtask
+                print('Task updated!')
+                user()
+    elif indata == 'view':
+        indata = input('Which hour would you like to view? (e.g. the first time, 0000 for midnight, 1300 for 1pm)')
+        for i in range(23):
+            if plan[i][0] == indata:
+                print('At', plan[i][0], 'you have the following task:', plan[i][1])
+                user()
+    elif indata == 'quit':
+        print('Thanks for using the daily planner! Goodbye!')
+        quit()
 user()
