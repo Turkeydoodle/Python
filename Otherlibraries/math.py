7d92afb0914ace6a1b43f689f44a9535bc8328ac
Kevin Todd-Yu <kevintoddyu@gmail.com>
2025-10-04 10:22:23 -0700

    changes to some other libraries

--- Otherlibraries/math.py
+++ Otherlibraries/math.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
-
-plt.plot(x, y)
-plt.title('Sine Wave')
-plt.xlabel('x values')
-plt.ylabel('sin(x)')
+x = np.sin(np.linspace(10, 100, 100))
+y = np.cos(x)
+z = np.tan(np.linspace(1, 10, 10))
+lsit = [1,2,3,4,5,6,7,8,9]
+lsit2 = bin(lsit[1])
+plt.plot(x, y,z)
+plt.legend(['X vs Y', 'Z'])
+plt.bar(lsit, lsit2)
+plt.title('Numpy and Matplotlib Plot')
+plt.xlabel('X Output')
+plt.ylabel('Y output')
 plt.grid(True)
 plt.show()
