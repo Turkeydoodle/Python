5c022ae936395683088fc24b2c8b547dbfee0611
Kevin Todd-Yu <kevintoddyu@gmail.com>
2025-10-11 12:31:58 -0400

    messing around with the bs4 library.

--- Otherlibraries/bsoup4.py
+++ Otherlibraries/bsoup4.py
@@ -2,5 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 response = requests.get("https://www.piday.org/million/")
 soup = BeautifulSoup(response.text, 'html.parser')
+print('Please memorize the following digits of Pi:')
 for text in soup.stripped_strings:
     print(text)
