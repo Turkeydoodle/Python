7e519a272ba494b9e68ecd6ebd1bfb806ca6587a
Kevin Todd-Yu <kevintoddyu@gmail.com>
2025-10-11 13:33:58 -0400

    Restructuring for better understanding

--- Otherlibraries/requesting.py
+++ Otherlibraries/requesting.py
@@ -5,3 +5,23 @@ response = get("https://www.google.com/realornot")
 print(str(response)+', because google.com/realornot does not exist')
 response = get("https://real-frogger.neocities.org/")
 print(str(response)+', because real-frogger.neocities.org does exist')
+
+def parse(text):
+    chomp('<', text)
+    website = parse_html(text)
+def chomp(text, body):
+    extract = body[:len(text)]
+    if extract == text:
+        return body[len(text):]
+    else:
+        raise ValueError("Expected " + text + ", got " + extract)
+
+#define parse head and parse body
+
+def parse_html(text):
+    chomp('<html>', text)
+    #parse_and_text = parse_head(text)
+    #parse = parse_body(text)
+    chomp('</html>', text)
+    #return {'head': parse_and_text, 'body': parse}
+parse(response.text)
--- hbs.py
+++
@@ -1,4 +0,0 @@
-print('This is a program to customize a birthday song for yourself or for others')
-bp = input('who is celebrating the birthday?')
-for i in range(4):
-    print(f'happy birthday to {bp}!!!')
