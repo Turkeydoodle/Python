b3b3d1a0478030d0f48762bc77e6047158776c6c
Kevin Todd-Yu <kevintoddyu@gmail.com>
2025-12-23 16:44:15 -0700

    Finishing the program

--- EmailPygame/main.py
+++ EmailPygame/main.py
@@ -5,6 +5,8 @@ timer = pygame.time.Clock()
 font = pygame.font.Font(None, 30)
 text_surface = font.render("Email", True, (0, 0, 0))
 done = False
+currentplace = 0
+inputed2 = ''
 x = -1000
 y = -1000
 inputed = ''
@@ -13,6 +15,7 @@ screenheight = 500
 window = pygame.display.set_mode( ( screenwidth,screenheight ) )
 username = pygame.surface.Surface((250, 50))
 password = pygame.surface.Surface((250, 50))
+selected = pygame.surface.Surface((260, 60), pygame.SRCALPHA)
 window.blit(username, (100, 100))
 pygame.display.set_caption( "Email" )
 fps = 30
@@ -20,7 +23,7 @@ background = pygame.image.load(r"\Users\user\OneDrive\Desktop\Personal\Productiv
 yv = 1
 xv = 1
 def renderhome():
-    global x, y, xv, yv
+    global x, y, xv, yv, inputed2
     x += xv
     y += yv
     xv += (0 - x) * 0.001
@@ -30,6 +33,10 @@ def renderhome():
     if y > 0 or y < -500:
         yv = -yv
     window.blit(background, (x, y))
+    username.fill((255,255,255))
+    password.fill((255,255,255))
+    username_pos = (125, 200)
+    password_pos = (125, 300)
     font = pygame.font.Font(None, 30)
     text_surface = font.render("Email   Log-in", True, (0, 0, 0))
     window.blit(text_surface, (10, 10))
@@ -41,10 +48,17 @@ def renderhome():
     window.blit(text_surface, (125, 175))
     text_surface = font.render("Password:", True, (0, 0, 0))
     window.blit(text_surface, (125, 275))
-    text_surface = font.render(inputed, True, (255, 255, 255))
+    text_surface = font.render(inputed, True, (0,0,0))
     window.blit(username, (125, 200))
-    window.blit(text_surface, (125, 212.5))
+    window.blit(text_surface, (125, 212))
+    text_surface = font.render('*'*len(inputed2), True, (0, 0, 0))
     window.blit(password, (125, 300))
+    window.blit(text_surface, (125, 320))
+    selected.fill((0,0,0,0))
+    pygame.draw.rect(selected, (0,0,0), selected.get_rect(), 3)
+    sel_x = username_pos[0] - 5
+    sel_y = username_pos[1] - 5 if currentplace == 0 else password_pos[1] - 5
+    window.blit(selected, (sel_x, sel_y))
 while done == False:
     renderhome()
     pygame.display.update()
@@ -52,12 +66,26 @@ while done == False:
     for event in pygame.event.get():
         if event.type == QUIT:
             done = True
-
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_BACKSPACE:
-                inputed = inputed[:-1]
+            if event.key != pygame.K_TAB and event.key != pygame.K_RETURN:
+                if currentplace == 0:
+                    if event.key == pygame.K_BACKSPACE:
+                        inputed = inputed[:-1]
+                    else:
+                        inputed += event.unicode
+                else:
+                    if event.key == pygame.K_BACKSPACE:
+                        inputed2 = inputed2[:-1]
+                    else:
+                        inputed2 += event.unicode
             else:
-                inputed += event.unicode
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            done = True
+                if event.key == pygame.K_TAB:
+                    if currentplace == 0:
+                        currentplace = 1
+                    else:
+                        currentplace = 0
+                if event.key == pygame.K_RETURN:
+                    done = True
+                    pygame.quit()
+print(inputed)
+print(inputed2)
