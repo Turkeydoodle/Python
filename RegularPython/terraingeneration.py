78e45802704da43f48e868f7df0e0d1bf490f82a
GudSkillsBadPC <kevin111411@icloud.com>
2026-05-01 19:33:29 -0500

    fixes to the terrain generation program.

--- RegularPython/terraingeneration.py
+++ RegularPython/terraingeneration.py
@@ -1,69 +1,108 @@
-import random 
+import random
+from turtle import *
 terrain_choices = ["grass", "dirt", "stone", "gravel"]
-terrain = [['', '', '', '', ''],['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', '']]
+terrain = []
+
+
 def generate(timestogenerate):
-    terrain[0][0] = terrain_choices[random.randint(0,3)]
+    global terrain
+    terrain = [[None for _ in range(timestogenerate)] for _ in range(timestogenerate)]
+
+    terrain[0][0] = random.choice(terrain_choices)
+
     if timestogenerate <= 5:
         for i in range(timestogenerate):
-            gchance = 25
-            dchance = 25
-            schance = 25
-            vchance = 25
-            if terrain[0][i-2] == "grass":
-                gchance += 20
-                vchance -= 20
-            elif terrain[0][i-2] == "dirt":
-                dchance += 20
-                schance -= 20
-            elif terrain[0][i-2] == "stone":
-                schance += 20
-                dchance -= 20
-            elif terrain[0][i-2] == "gravel":
-                vchance += 20
-                gchance -= 20
-            random_choice = random.randint(1, 100)
-            if random_choice <= gchance:
-                terrain[0][i-1] = "grass"
-            elif random_choice <= gchance+dchance:
-                terrain[0][i-1] = 'dirt'
-            elif random_choice <= gchance+dchance+schance:
-                terrain[0][i-1] = "stone"
-            elif random_choice <= gchance+dchance+schance+vchance:
-                terrain[0][i-1] = "gravel"
+            gchance = dchance = schance = vchance = 25
+
+            if i >= 2:
+                prev = terrain[0][i-2]
+                if prev == "grass":
+                    gchance += 20; vchance -= 20
+                elif prev == "dirt":
+                    dchance += 20; schance -= 20
+                elif prev == "stone":
+                    schance += 20; dchance -= 20
+                elif prev == "gravel":
+                    vchance += 20; gchance -= 20
+
+            r = random.randint(1, 100)
+            if r <= gchance:
+                terrain[0][i] = "grass"
+            elif r <= gchance + dchance:
+                terrain[0][i] = "dirt"
+            elif r <= gchance + dchance + schance:
+                terrain[0][i] = "stone"
+            else:
+                terrain[0][i] = "gravel"
+
     else:
-        columnstomake = min(timestogenerate, 5)
-        if columnstomake <= 5:
-            for i in range(columnstomake):
-                terrain[i][0] = terrain_choices[random.randint(0,3)]
-                for j in range(timestogenerate):
-                    gchance = 25
-                    dchance = 25
-                    schance = 25
-                    vchance = 25
-                    if terrain[i][j-2] == "grass":
-                        gchance += 20
-                        vchance -= 20
-                    elif terrain[i][j-2] == "dirt":
-                        dchance += 20
-                        schance -= 20
-                    elif terrain[i][j-2] == "stone":
-                        schance += 20
-                        dchance -= 20
-                    elif terrain[i][j-2] == "gravel":
-                        vchance += 20
-                        gchance -= 20
-                    random_choice = random.randint(1, 100)
-                    if random_choice <= gchance:
-                        terrain[0][i-1] = "grass"
-                    elif random_choice <= gchance+dchance:
-                        terrain[0][i-1] = 'dirt'
-                    elif random_choice <= gchance+dchance+schance:
-                        terrain[0][i-1] = "stone"
-                    elif random_choice <= gchance+dchance+schance+vchance:
-                        terrain[0][i-1] = "gravel"    
-def print_terrain():
-    for i in range(len(terrain)):
-        for j in range(len(terrain[i-1])):
-            print(terrain[i-1][j-1])
-generate(10)
-print_terrain()
+        for i in range(timestogenerate):
+            terrain[i][0] = random.choice(terrain_choices)
+
+            for j in range(1, timestogenerate):
+                gchance = dchance = schance = vchance = 25
+
+                if j >= 2:
+                    prev = terrain[i][j-2]
+                    if prev == "grass":
+                        gchance += 20; vchance -= 20
+                    elif prev == "dirt":
+                        dchance += 20; schance -= 20
+                    elif prev == "stone":
+                        schance += 20; dchance -= 20
+                    elif prev == "gravel":
+                        vchance += 20; gchance -= 20
+
+                r = random.randint(1, 100)
+                if r <= gchance:
+                    terrain[i][j] = "grass"
+                elif r <= gchance + dchance:
+                    terrain[i][j] = "dirt"
+                elif r <= gchance + dchance + schance:
+                    terrain[i][j] = "stone"
+                else:
+                    terrain[i][j] = "gravel"
+
+
+def render_terrain():
+    tile = 20
+    grid = len(terrain)
+
+    speed(0)
+    penup()
+    goto(-475, 300)  # Start inside the visible window
+    pendown()
+
+    for i in range(grid):
+        for j in range(grid):
+
+            t = terrain[i][j]
+
+            if t == "grass":
+                color("green")
+            elif t == "dirt":
+                color("brown")
+            elif t == "stone":
+                color("gray")
+            elif t == "gravel":
+                color("light gray")
+            else:
+                color("white")
+
+            begin_fill()
+            for _ in range(4):
+                forward(tile)
+                right(90)
+            end_fill()
+
+            forward(tile)
+
+        backward(grid * tile)
+        right(90)
+        forward(tile)
+        left(90)
+
+
+generate(50)
+render_terrain()
+done()
