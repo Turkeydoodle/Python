613d1927e1ab9bc05134f9d77d7fe391908aa9d3
GudSkillsBadPC <kevin111411@icloud.com>
2026-06-13 11:43:15 -0700

    made the game 2 sided, have not checked for bugs

--- RegularPython/tictactoe2.py
+++ RegularPython/tictactoe2.py
@@ -1,5 +1,7 @@
-win = False
-playerpieces = []
+player1win = False
+player2win = False
+playerpieces1 = []
+playerpieces2 = []
 def createboard():
     global board
     board = []
@@ -13,9 +15,12 @@ def printboard():
     print("1 {} | {} | {}".format(board[3], board[4], board[5]))
     print("-----------")
     print("2 {} | {} | {}".format(board[6], board[7], board[8]))
-def userinput():
+def userinput(player):
     global board
-    user = input("Where do you want to place your piece? (row, column):")
+    if player == 1:
+        user = input("Where do you want to place your piece, player 1? (row, column):")
+    else:
+        user = input("Where do you want to place your piece, player 2? (row, column):")
     try:
         row, column = user.split(",")
         row = int(row)
@@ -28,22 +33,33 @@ def userinput():
             print("Sorry, but your selected square is already taken.")
             userinput()
         else:
-            board[index] = "X"
-            playerpieces.append(index)
+            if player == 1:
+                board[index] = "X"
+                playerpieces1.append(index)
+            else:
+                board[index] = "O"
+                playerpieces2.append(index)
     except:
         print("Invalid input. Please enter row and column separated by a comma. (for example, 0,1)")
         return userinput()
 def checkforwin():
-    global win
+    global player1win, player2win
     winconditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for i in range(8):
-        if winconditions[i-1][0] in playerpieces and winconditions[i-1][1] in playerpieces and winconditions[i-1][2] in playerpieces:
-            win = True
+        if winconditions[i-1][0] in playerpieces1 and winconditions[i-1][1] in playerpieces1 and winconditions[i-1][2] in playerpieces1:
+            player1win = True
 def main():
-    while win == False:
-        userinput()
+    while player1win == False and player2win == False:
+        playernumber = 1
+        if playernumber == 1:
+            userinput(1)
+            playernumber = 2
+        else:
+            userinput(2)
+            playernumber = 1
         printboard()
-        print(playerpieces)
+        print("Player 1 pieces: "+playerpieces1)
+        print("Player 2 pieces: "+playerpieces2)
         checkforwin()
     else:
         print("Win detected, player has won.")
